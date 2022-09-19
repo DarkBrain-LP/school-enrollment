9a7bd82dd78a35193f0c88699a3aca40a2c45c24
YOVO Yawo Emmanuel <emmanuelyovo649@gmail.com>
2022-09-19 13:11:08 +0200

    added jdbc

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,21 +4,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+// Importing database
+import java.sql.*;
+// Importing required classes
+import java.util.*;
+
 public class Main {
     private static String schoolName = "LP-School";
     private static int userSelection;
-    private static boolean selectedInrollment = false;
-    private static String users ;
+    private static boolean selectedEnrollment = false;
+    private static String users;
 
     private static Scanner sc = new Scanner(System.in);
 
-    public Date readDate() throws Exception{
+    public Date readDate() throws Exception {
         String dateFormat = "dd/MM/yyyy";
         Scanner scanner = new Scanner(System.in);
         return new SimpleDateFormat(dateFormat).parse(scanner.nextLine());
     }
 
-    public static void showMainMenue(){
+    public static void showMainMenue() {
         System.out.println("Bienvenue sur la plateforme d'inscription à" + schoolName);
 
         System.out.println("\n\n\nSelectionnez l'opération que vous voulez faire");
@@ -26,14 +31,14 @@ public class Main {
         System.out.println("2: Connexion");
     }
 
-    public static int menueOperation(){
-        selectedInrollment = true;
+    public static int menueOperation() {
+        selectedEnrollment = true;
         userSelection = sc.nextInt();
         System.out.println("Vous avez sélectionné l'option " + userSelection);
         return userSelection;
     }
 
-    public static void enrollmentOperation(){
+    public static void enrollmentOperation() {
         String username = sc.nextLine();
         String password = sc.nextLine();
         String name = sc.nextLine();
@@ -41,7 +46,7 @@ public class Main {
         int age = sc.nextInt();
     }
 
-    public static boolean loginOperation(){
+    public static boolean loginOperation() {
         String username = sc.nextLine();
         String password = sc.nextLine();
 
@@ -50,12 +55,40 @@ public class Main {
 
 
 
-    public static void main(String[] args){
-        showMainMenue();
-        menueOperation();
+    public static void main(String[] args)throws Exception
+    {
+        String url = "jdbc:mysql://localhost:3306/ecole"; // table details
+        String username = "root"; // MySQL credentials
+        String password = "";
 
-        if(selectedInrollment){
+        Class.forName("com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(url, username, password);
+        System.out.println("Connection Established successfully");
 
-        }
+        String query = "select * from classe"; // query to be run
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query); // Execute query
+        rs.next();
+        String className = rs.getString("nomclasse"); // Retrieve class name from db
+        int classID = rs.getInt("idclasse");
+
+        System.out.println("idClass: " + classID + "\nclassName: " + className); // Print result on console
+
+        // Reading a new line
+        rs.next();
+        className = rs.getString("nomclasse"); // Retrieve class name from db
+        classID = rs.getInt("idclasse");
+        System.out.println("idClass: " + classID + "\nclassName: " + className); // Print result on console
+
+        // Reading a new line
+        rs.next();
+        className = rs.getString("nomclasse"); // Retrieve class name from db
+        classID = rs.getInt("idclasse");
+        System.out.println("idClass: " + classID + "\nclassName: " + className); // Print result on console
+
+        st.close(); // close statement
+        con.close(); // close connection
+        System.out.println("Connection Closed....");
     }
 }
